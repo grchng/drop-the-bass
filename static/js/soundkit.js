fcda5771f5a7fc1b129645047491e3becc5b4273
Grace Chung <gcw.chung@gmail.com>
2020-03-11 04:54:06 -0700

    Modified JS

--- static/js/soundkit.js
+++ static/js/soundkit.js
@@ -1,5 +1,7 @@
 "use strict";
 // Play Audio
+"use strict";
+// Play Audio
 window.addEventListener('click', clickSound);
 window.addEventListener('keydown', tapSound);
 
@@ -9,50 +11,35 @@ const playSound = function (audio) {
     audio.volume = 0.4;
     audio.play();
 }
-const addLightEffects = function (key, audio) {
+
+const addLightEffects = function (key) {
     key.classList.add('playing'); // add class="playing" to key element
-    audio.classList.add('playing');
+    // audio.classList.add('playing');
+    console.log("addlighteffects", key);
 }
 
 function tapSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     playSound(audio);
-
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    addLightEffects(key, audio);
+    addLightEffects(key);
 }
 
 function clickSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.target.id}"]`);
     playSound(audio);
-    
     const key = document.querySelector(`.key[data-key="${e.target.id}"]`);
-    addLightEffects(key, audio);
+    addLightEffects(key);
 }
 
 function removeTransition(e) {
-        // var x = document.getElementById("myAudio").ended; -- audio object
-        // if x = true 
-        // then go to that div and remove the playing class
-        // if (e.srcElement.paused == "true") {
-        // this.classList.remove('playing');
-    
-    console.log('event', e);
-    console.log('this:', this);
+    if (e.propertyName !== 'transform') return;
+    console.log(e);
+    this.classList.remove('playing');
 }
 
 const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend',removeTransition));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 
 
 
-// code for muting audio
-// const rees = document.getElementById('audio-col0').querySelectorAll(".col0");
-// rees.forEach(ree => ree.muted="true");
-// console.log(rees);
-
-const muteAudio = function (col) {
-    console.log(col);
-    const rees = document.getElementById(`audio-${col}`).querySelectorAll(`.${col}`)
-}
-// muteAudio("col0");
